1736bdbae4f2bf9d9e5f63e23be3979b0a92131c
Nickolas <nickolasram@gmail.com>
2024-06-22 19:51:19 -0700

    restructuring

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,37 +1,6 @@
-import {MongoClient} from 'mongodb';
+import readProjects from './functions';
 
-
-const uri = process.env.NEXT_PUBLIC_MONGODB
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
- }
-
-async function connectToCluster() {
-    let mongoClient;
-    try {
-        mongoClient = await MongoClient.connect(uri, options);
-        return mongoClient;
-    } catch (error) {
-        console.error('Connection to MongoDB Atlas failed!', error);
-        process.exit();
-    }
-}
-async function readProjects() {
-    let mongoClient;
-    try {
-        mongoClient = await connectToCluster();
-        const db = mongoClient.db('NSCAD_Practicum_site');
-        const collection = db.collection('Projects');
-        // let queryresult = await collection.find({}).toArray();
-        let queryresult = await collection.find({title: 'Pelletier Construction Group'}).toArray();
-        return queryresult;
-    } finally {
-        await mongoClient.close();
-    }
-}
-const aaa = await readProjects();
-
-export default function Page() {
+export default async function Page() {
+    const aaa = await readProjects();
     return ( <p>{aaa[0].title}</p>);
 }
