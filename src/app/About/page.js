bcc1a09df36a0bddd54dbc23610764043493870b
Nickolas <nickolasram@gmail.com>
2024-06-23 00:34:25 -0700

    env test

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -2,5 +2,6 @@ import readProjects from './functions';
 
 export default async function Page() {
     const aaa = await readProjects();
-    return ( <p>{aaa[0].title}</p>);
+    // return ( <p>{aaa[0].title}</p>);
+    return ( <p>{process.env.NEXT_PUBLIC_MONGODB}</p>);
 }
