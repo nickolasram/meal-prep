7d270f5a57d81f2218fe6bf00339ae0919049c80
Nickolas <nickolasram@gmail.com>
2024-06-24 22:24:34 -0700

    env test 13

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -3,6 +3,6 @@
 export default async function Page() {
     // const aaa = await readProjects();
     // return ( <p>{aaa[0].title}</p>);
-    const a = process.env.NEXT_PUBLIC_MONGODB ? "aa" : "bb"
+    const a = process.env.NEXT_PUBLIC_MONGODB ? process.env.NEXT_PUBLIC_MONGODB : "bb"
     return ( <p>env variable: {a}</p>);
 }
