325d9c1eb8a5aaec470809433d8b9fd47aab6bc8
Nickolas <nickolasram@gmail.com>
2024-06-24 18:17:17 -0700

    force rebuild

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -3,5 +3,5 @@
 export default async function Page() {
     // const aaa = await readProjects();
     // return ( <p>{aaa[0].title}</p>);
-    return ( <p>env secret: {process.env.NEXT_PUBLIC_MONGODB}</p>);
+    return ( <p>env: {process.env.NEXT_PUBLIC_MONGODB}</p>);
 }
